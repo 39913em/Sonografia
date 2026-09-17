bdb31fbc27a86c8bc3a7f187514f1bfe73d1db90
39913 <47995483+39913em@users.noreply.github.com>
2026-09-17 17:29:56 -0600

    agrega link manifest + registra sw

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => self.clients.claim());
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+const CACHE = "sonografia-v1";
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./","./index.html","./manifest.json"])));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
