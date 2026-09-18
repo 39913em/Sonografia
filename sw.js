e416383ac569e9924ebfa7473c0db93ef7973da2
39913 <47995483+39913em@users.noreply.github.com>
2026-09-17 20:14:09 -0600

    refresh

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-const CACHE = "sonografia-v1";
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./","./index.html","./manifest.json"])));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('install', e=>self.skipWaiting());
+self.addEventListener('activate', e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c))))); self.clients.claim()});
